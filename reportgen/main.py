c330dfee3fe5a086411080cacd763ac2c5f48716
JSong <gasongjian@126.com>
2017-01-20 00:11:24 +0800

    Update main.py

--- reportgen/main.py
+++ reportgen/main.py
@@ -31,6 +31,42 @@ def chi2_test(df,alpha=0.5):
     return chi2_data
 
 
+def rptcrosstab(data_index,data_column,code=code[qq]):
+    '''
+    默认data_column=data[cross_class] 是交叉分析列标签对应的数据，列数==1
+    code是data_index 的编码，列数大于1
+    '''
+    # 单选题
+    qtype=code['qtype']
+    columns=data_column[data_column.notnull()].unique()
+    index=code['qlist']
+    len_index=d2[current_name].notnull().sum()
+    len_column=data_column.notnull().sum()
+    index_freq=data[cross_class].value_counts()
+    cn[u'总体']=cn.sum()
+    if qtype == u'单选题':
+        t=pd.crosstab(data_index,data_column)
+    elif qtype == u'多选题':
+        t=data.groupby([cross_class])[qlist].sum().T
+    elif qtype == u'矩阵单选题':
+        t=d2.groupby([cross_class])[qlist].mean().T
+    elif qtype == u'排序':
+        topn=max([len(data_index[q][data_index[q].notnull()].unique()) for q in index])
+        t=pd.DataFrame(columns=columns,index=index)
+        for i in t.index:
+            for j in t.columns:
+                tmp=data_index.loc[data_column==j,i]
+                tmp=tmp*(topn+1-tmp)
+                t.loc[i,j]=tmp.sum()
+    t.rename(index=code['code'],inplace=True)
+
+
+
+
+
+
+
+
 def contingency(df,col_dis=None,row_dis=None,alpha=0.05):
     '''
     列联表分析：
@@ -40,6 +76,7 @@ def contingency(df,col_dis=None,row_dis=None,alpha=0.05):
     需要注意的问题：
     1、题目分为单选(互斥变量)和多选(非互斥变量)，非互斥变量无形中增加了总样本量，默认把需
     要检验的变量放在列中，且要求是互斥变量,如若行是非互斥变量，则需要提供互斥变量的样本频数表
+    默认行是qq，列是cross_class
     输入：
     col_dis: 列变量的频数分布
     row_dis: 行变量的频数分布
@@ -181,200 +218,23 @@ for qq in cross_qlist:
     '''
 
     qtitle=code[qq]['content']
-    qlist=code[qq]['qlist']    
+    qlist=code[qq]['qlist']
     if 'sample_len' in code[qq]:
         sample_len=code[qq]['sample_len']
     else:
         # ===========================此处待定================================================
         sample_len=len(data)
-    t=rptcrosstab(data[qlist],data[cross_class],qtype=code[qq]['qtype'])
+    t=rptcrosstab(data[qlist],data[cross_class],code=code[qq])
     t.rename(index=code[qq]['code'],inplace=True)
     cc=contingency(t,col_dis=None,row_dis=None,alpha=0.05)
+
+
+
+
     if ('name' in code[qq].keys()) and code[qq]['name'] in [u'满意度','satisfaction']:
         title=u'整体满意度'
-    
-    
-    if (u'满意度' in qtitle) and (u'整体' in qtitle):
-        qtype2=u'整体满意度'
-        t=d2.groupby([cross_class])[[qq]].mean()
-        chi2_data=chi2_test(pd.crosstab(d2[qq],d2[cross_class]))
-        title=u'整体满意度'
-        if chi2_data[0]:
-            difference[qq]=1
-            summary=u'卡方检验 p值为 %s <0.05, 即存在显著性差异' %(chi2_data[1])
-        else:
-            difference[qq]=0
-            summary=u'卡方检验 p值为 %.2f >0.05, 即不存在显著性差异' %(chi2_data[1])
-        rpt.plot_chart(prs,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-        rpt.plot_chart(prs1,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-        continue
-
-    if (u'意愿' in qtitle) and (u'推荐' in qtitle):
-        qtype2='NPS'
-        t=pd.crosstab(d2[qq],d2[cross_class])
-        chi2_data=chi2_test(t)
-        t[u'总体']=d2[qq].value_counts()
-        for i in range(len(t.columns)):
-            t.iloc[:,i]=t.iloc[:,i]/cn[t.columns[i]]
-        NPS=t.iloc[9:11,:].sum()-t.iloc[0:7,:].sum()
-        NPS=pd.DataFrame(NPS)
-        NPS.rename(columns={0:'NPS'},inplace=True)
-        title=u'NPS'
-        if chi2_data[0]:
-            difference[qq]=1
-            summary=u'卡方检验 p值为 %s <0.05, 即存在显著性差异' %(chi2_data[1])
-        else:
-            difference[qq]=0
-            summary=u'卡方检验 p值为 %.2f >0.05, 即不存在显著性差异' %(chi2_data[1])
-        rpt.plot_chart(prs,NPS,'COLUMN_CLUSTERED',title=title,summary=summary)
-        rpt.plot_chart(prs1,NPS,'COLUMN_CLUSTERED',title=title,summary=summary)
-        continue
-    '''
-    if (u'城市' in qtitle) and (code[qq]['qtype'] == u'下拉填空题'):
-        qtype2=u'城市'
-        # 省份
-        t=pd.crosstab(d2[qlist[0]],d2[cross_class])
-        title=qq+': '+code[qq]['content']+u'_省份'
-        summary=u'不满足卡方检验条件们，暂时不提供显著性检验.'
-        if len(t)>max_column_chart:
-            rpt.plot_chart(prs,t,'BAR_CLUSTERED',title=title,summary=summary)
-            rpt.plot_chart(prs1,t,'BAR_CLUSTERED',title=title,summary=summary)
-        else:
-            rpt.plot_chart(prs,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-            rpt.plot_chart(prs1,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-        # 城市
-        t=pd.crosstab(d2[qlist[1]],d2[cross_class])
-        title=qq+': '+code[qq]['content']+u'_城市'
-        summary=u'不满足卡方检验条件们，暂时不提供显著性检验.'
-        if len(t)>max_column_chart:
-            rpt.plot_chart(prs,t,'BAR_CLUSTERED',title=title,summary=summary)
-            rpt.plot_chart(prs1,t,'BAR_CLUSTERED',title=title,summary=summary)
-        else:
-            rpt.plot_chart(prs,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-            rpt.plot_chart(prs1,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-        continue
-    '''
-
-
-    if code[qq]['qtype'] in [u'单选题',u'多选题']:
-        if code[qq]['qtype'] == u'单选题':
-            t=pd.crosstab(d2[qq],d2[cross_class])
-            t[u'总体']=d2[qq].value_counts()
-        else:
-            qlist=code[qq]['qlist']
-            if re.findall('_open',code[qq]['qlist'][-1]):
-                qlist=qlist[:-1]
-            t=d2.groupby([cross_class])[qlist].sum().T
-            t[u'总体']=d2[qlist].sum()
-
-        '''t
-        qq\cross_class| B1 | B2 | B3 | 总体
-        A1 |          n11  |n12 |n13 |
-        A2 |          n21 |n22  |n23 |
-        '''
-        if 'code' in code[qq]:
-            t.rename(index=code[qq]['code'],inplace=True)
-        # 小样本剔除
-        #t=t[t[u'总体']>=30] #剔除样本量小于30的行指标
-        if u'其他【请注明】' in t.index:
-            t.drop([u'其他【请注明】'],axis=0,inplace=True)
-        if u'其他' in t.index:
-            t.drop([u'其他'],axis=0,inplace=True)
-        if delclass:
-            t.drop([delclass],axis=1,inplace=True)
 
 
 
-        cc=contingency(t,col_dis=None,row_dis=None,alpha=0.05)
-        t1=cc[data_style]
-
-        # title
-        title=qq+': '+code[qq]['content']
-        # summary
-        if cc[chi_test]==1:
-            difference[qq]=1
-            summary=u'卡方检验 p值为 %s <0.05, 即存在显著性差异' %(cc['coef']['p_value'])
-        elif cc[chi_test]==0
-            difference[qq]=0
-            summary=u'卡方检验 p值为 %.2f >0.05, 即不存在显著性差异' %(cc['coef']['p_value'])
-        else:
-            difference[qq]=-1
-            summary=u'不满足卡方检验的条件'
-        if cc['significant']:
-            tmp=str(cc['significant'])
-            summary=summary+'\n'+u'相对而言，显著的有：'+ tmp[1:-1]
-
-        # footnote
-        footnote=u'数据来源于'+qq+',样本N=%s'%row_len
-
-        # 把t1绘制到ppt上
-        if len(t1)>max_column_chart:
-            rpt.plot_chart(prs,t1,'BAR_CLUSTERED',title=title,summary=summary,footnote=footnote)
-        else:
-            rpt.plot_chart(prs,t1,'COLUMN_CLUSTERED',title=title,summary=summary,footnote=footnote)
-        continue
-
-    if code[qq]['qtype'] in [u'排序题']:
-        qlist=code[qq]['qlist']
-        for c in cross_class_list:
-            dd=d2[d2[cross_class]==c]
-            t=pd.DataFrame()
-            for q in qlist:
-                tmp=dd[q].value_counts()
-                t=pd.concat([t,tmp],axis=1)
-            t.rename(index={1:'TOP1',2:'TOP2',3:'TOP3',4:'TOP4',5:'TOP5'},inplace=True)
-            t.rename(columns=code[qq]['code'],inplace=True)
-            t.replace({np.nan:0},inplace=True)
-            t=t.T
-            t.sort_values(by=['TOP1'],ascending=0,inplace=True)
-            title=qq+': '+u'排序题__'+c
-            summary=u'不满足卡方检验条件们，暂时不提供显著性检验.'
-            if len(t)>max_column_chart:
-                rpt.plot_chart(prs,t,'BAR_STACKED',title=title,summary=summary)
-                rpt.plot_chart(prs1,t,'BAR_STACKED',title=title,summary=summary)
-            else:
-                rpt.plot_chart(prs,t,'COLUMN_STACKED',title=title,summary=summary)
-                rpt.plot_chart(prs1,t,'COLUMN_STACKED',title=title,summary=summary)
-            continue
-
-    if code[qq]['qtype'] in [u'矩阵单选题']:
-        #此处将顺序因子变量处理成连续数值变量，可能会有些不妥
-        qlist=code[qq]['qlist']
-        t=d2.groupby([cross_class])[qlist].mean().T
-        t.rename(index=code[qq]['code_r'],inplace=True)
-        title=qq+': '+code[qq]['content']
-        summary=u'不满足卡方检验条件们，暂时不提供显著性检验.'
-        if len(t)>max_column_chart:
-            rpt.plot_chart(prs,t,'BAR_CLUSTERED',title=title,summary=summary)
-            rpt.plot_chart(prs1,t,'BAR_CLUSTERED',title=title,summary=summary)
-        else:
-            rpt.plot_chart(prs,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-            rpt.plot_chart(prs1,t,'COLUMN_CLUSTERED',title=title,summary=summary)
-        continue
 
 
-# ==============小结页
-difference=pd.Series(difference,index=total_qlist_0)
-
-n1=sum(difference==1)
-n2=sum(difference==0)
-chi2_test_yes=list(difference[difference==1].index)
-
-title=u'小结'
-
-summary=u'一共卡方检验了 %s 道题, 其中有 %s 道题呈现显著性差异.'%(n1+n2,n1)
-summary=summary+'\n'+u'有显著性差异的分别是：'
-summary=summary+'\n'+', '.join(chi2_test_yes)
-summary=summary+'\n'+u'具体细节可查看文件: _显著性检验.csv'
-
-#rpt.plot_textbox(prs,title=title,summary=summary)
-#rpt.plot_textbox(prs1,title=title,summary=summary)
-
-# ========================文件生成和导出
-difference.name=cross_class
-difference=pd.DataFrame(difference)
-if not os.path.exists('.\\out'):
-    os.mkdir('.\\out')
-difference.to_csv('.\\out\\'+filename+u'_显著性检验.csv',encoding='gbk')
-prs.save('.\\out\\'+filename+u'_百分比.pptx')
-prs1.save('.\\out\\'+filename+u'_TGI指数.pptx')
