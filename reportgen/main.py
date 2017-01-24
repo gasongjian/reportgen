41d1f82c3816b605784cba1c3bf3fbeb02eadbbf
JSong <gasongjian@126.com>
2017-01-24 17:49:22 +0800

    Update main.py

--- reportgen/main.py
+++ reportgen/main.py
@@ -28,7 +28,7 @@ filename='russia'
 if not os.path.exists('.\\out'):
     os.mkdir('.\\out')
 #Writer=pd.ExcelWriter('.\\out\\'+filename+'.xlsx')
-Writer2=pd.ExcelWriter('.\\out\\'+filename+'_twoway.xlsx')
+Writer2=pd.ExcelWriter('.\\out\\'+filename+'_chi.xlsx')
 cross_qlist=code[cross_class]['qlist']
 for qq in cross_list:    
     if code[qq]['qtype'] == u'单选题':
@@ -37,19 +37,22 @@ for qq in cross_list:
         t1.rename(columns=code[qq]['code'],inplace=True)
         t.fillna(0,inplace=True)
         t1.fillna(0,inplace=True)
-        tt1=t1.copy()
-        tt1.drop(u'总体',axis=1,inplace=True)
         t1.drop(u'总体',axis=1,inplace=True)
-        sample_len=tt1.sum().sum()*1.0
-        for i in tt1.index:
-            for c in tt1.columns:
-                tt1.loc[i,c]=tt1.loc[i,c]-t1.loc[i,:].sum()*t1.loc[:,c].sum()/sample_len
+        fe=t1.copy()
+        N=fe.sum().sum()
+        for i in fe.index:
+            for j in fe.columns:
+                fe.loc[i,j]=fe.loc[i,:].sum()*fe.loc[:,j].sum()/N
+        TSI=t1-fe
+        chi=(t1-fe)*(t1/fe-1)*(TSI.applymap(lambda x: int(x>0))*2-1)
+       
+
         #for c in t.columns:
         #    t1[c]=t[c]/t[u'总体']
         #t1.loc[u'sign']=t1.mean()+2*t1.std()
         #t.loc[u'sign']=t1.loc[u'sign'].copy()
         #t2=pd.concat([t,t1],axis=1)
         #t2.to_excel(Writer,qq)
-        tt1.to_excel(Writer2,qq)
+        chi.to_excel(Writer2,qq)
 #Writer.save()
 Writer2.save()
