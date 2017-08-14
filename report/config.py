531fc2d38767ade810ebe5849e2cc25d8ccda27a
JSong <gasongjian@126.com>
2017-08-14 18:03:39 +0800

    Create config.py

---
+++ report/config.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 09:39:10 2017
+
+@author: JSong
+"""
+
+# PPT图表中的数字位数
+number_format_chart_1='0"%"'
+number_format_chart_2='0.00'
+
+# PPT图表中坐标轴的数字标签格式
+number_format_chart_label='0"%"'
+
+#  默认字体大小
+'''
+Pt(8):101600,  Pt(10):127000,  Pt(12):152400,  Pt(14):177800
+Pt(16):203200,  Pt(18):228600,  Pt(20):254000,  Pt(22):279400
+Pt(24):304800,  Pt(26):330200
+'''
+font_default_size=127000# Pt(10)
+
+
+#  PPT中结论文本框所在的位置
+# 四个值依次为left、top、width、height
+summary_loc=[0.10,0.14,0.80,0.15]
+
+
+#  PPT中结论文本框所在的位置
+# 四个值依次为left、top、width、height
+chart_loc=[0.10,0.30,0.80,0.60]
+
+
