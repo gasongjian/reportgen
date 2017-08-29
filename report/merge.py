3e7158c516136de9a82e9f66f7204193d0dd63d5
JSong <gasongjian@126.com>
2017-08-29 17:42:02 +0800

    Update merge.py

--- report/merge.py
+++ report/merge.py
@@ -60,7 +60,7 @@ def code_to_text(code):
         code1[c]=value
     return  code1             
     
-
+'''
 def code_similar(code1,code2):
     code11=code_to_text(code1)
     code22=code_to_text(code2)
@@ -81,6 +81,87 @@ def code_similar(code1,code2):
         code_distance_min.loc[c1,'distance']=distance
         code_distance_min.loc[c1,'similar']=rate
     return code_distance_min                             
+'''
+
+
+def code_similar(code1,code2):
+    '''
+    题目内容相似度用最小编辑距离来度量
+    选项相似度分为几种
+    1、完全相同：1
+    2、单选题：暂时只考虑序号和值都相等的，且共同变量超过一半:2
+    2、多选题/排序题：不考虑序号，共同变量超过一半即可：3
+    3、矩阵单选题：code_r 暂时只考虑完全匹配
+    4、其他情况为0
+    
+    '''
+    code_distance_min=pd.DataFrame(index=code1.keys(),columns=['qnum','similar_content','similar_code'])
+    for c1 in code1:
+        # 计算题目内容的相似度
+        disstance_str=pd.Series(index=code2.keys())
+        for c2 in code2:
+            if code1[c1]['qtype']==code2[c2]['qtype']:
+                disstance_str[c2]=levenshtein(code1[c1]['content'], code2[c2]['content'])
+        c2=disstance_str.idxmin()       
+        if '%s'%c2 == 'nan':            
+            continue
+        min_len=(len(code1[c1]['content'])+len(code2[c2]['content']))/2
+        similar_content=100-100*disstance_str[c2]/min_len if min_len>0 else 0
+        # 计算选项的相似度                                     
+        qtype=code2[c2]['qtype']
+        if qtype == '单选题':
+            t1=code1[c1]['code']
+            t2=code2[c2]['code']
+            inner_key=list(set(t1.keys())&set(t2.keys()))
+            tmp=all([t1[c]==t2[c] for c in inner_key])
+            if t1==t2:
+                similar_code=1
+            elif len(inner_key)>=0.5*len(set(t1.keys())|set(t2.keys())) and tmp:
+                similar_code=2
+            else:
+                similar_code=0
+        elif qtype in ['多选题','排序题']:
+            t1=code1[c1]['code']
+            t2=code2[c2]['code']
+            t1=[t1[c] for c in code1[c1]['qlist']]
+            t2=[t2[c] for c in code2[c2]['qlist']]
+            inner_key=set(t1)&set(t2)
+            if t1==t2:
+                similar_code=1
+            elif len(set(t1)&set(t2))>=0.5*len(set(t1)|set(t2)):
+                similar_code=3               
+            else:
+                similar_code=0
+        elif qtype in ['矩阵多选题']:
+            t1=code1[c1]['code_r']
+            t2=code2[c2]['code_r']
+            t1=[t1[c] for c in code1[c1]['qlist']]
+            t2=[t2[c] for c in code2[c2]['qlist']]
+            inner_key=set(t1)&set(t2)
+            if t1==t2:
+                similar_code=1
+            elif len(set(t1)&set(t2))>=0.5*len(set(t1)|set(t2)):
+                similar_code=3               
+            else:
+                similar_code=0
+        elif qtype in ['填空题']:
+            similar_code=1
+        else:
+            similar_code=0
+
+        code_distance_min.loc[c1,'qnum']=c2
+        code_distance_min.loc[c1,'similar_content']=similar_content
+        code_distance_min.loc[c1,'similar_code']=similar_code
+
+
+    # 剔除qnum中重复的值
+    code_distance_min=code_distance_min.sort_values(['qnum','similar_content','similar_code'],ascending=[False,False,True])
+    code_distance_min.loc[code_distance_min.duplicated(['qnum']),:]=np.nan
+    code_distance_min=pd.DataFrame(code_distance_min,index=code1.keys())                             
+    return code_distance_min 
+
+
+
 
 def data_merge(ques1,ques2,qlist1=None,qlist2=None,name1='ques1',name2='ques2',\
                mergeqnum='Q0',similar_threshold=70):
@@ -94,67 +175,143 @@ def data_merge(ques1,ques2,qlist1=None,qlist2=None,name1='ques1',name2='ques2',\
     if (qlist1 is None) or (qlist2 is None):
         qlist1=[]
         qlist2=[]
+        qqlist1=[]
+        qqlist2=[]
         code_distance_min=code_similar(code1,code2)
         code1_key=sorted(code1,key=lambda x:int(re.findall('\d+',x)[0]))
         for c1 in code1_key:
             qtype1=code1[c1]['qtype']
             #print('{}:{}'.format(c1,code1[c1]['content']))
             rs_qq=code_distance_min.loc[c1,'qnum']
-            similar=code_distance_min.loc[c1,'similar']
-            if similar==100:
+            similar_content=code_distance_min.loc[c1,'similar_content']
+            similar_code=code_distance_min.loc[c1,'similar_code']
+            if (similar_content>=similar_threshold) and (similar_code in [1,2]):
                 #print('推荐合并第二份数据中的{}({}), 两个题目相似度为为{:.0f}%'.format(rs_qq,code2[rs_qq]['content'],similar))
                 print('将自动合并: {} 和 {}'.format(c1,rs_qq))
                 user_qq=rs_qq
+                qqlist1+=code1[c1]['qlist']
+                qqlist2+=code2[user_qq]['qlist']
                 qlist1.append(c1)
-                qlist2.append(user_qq)
-            elif similar>=70:
+                qlist2.append(rs_qq)
+            elif (similar_content>=similar_threshold) and (similar_code==3):
+                # 针对非单选题，此时要调整选项顺序
+                t1=code1[c1]['code_r'] if qtype1 =='矩阵单选题' else code1[c1]['code']
+                t1_qlist=code1[c1]['qlist']
+                t1_value=[t1[k] for k in t1_qlist]
+                t2=code2[rs_qq]['code_r'] if qtype1 =='矩阵单选题' else code2[rs_qq]['code']
+                t2_qlist=code2[rs_qq]['qlist']
+                t2_value=[t2[k] for k in t2_qlist]
+                # 保留相同的选项
+                t1_qlist_new=[q for q in t1_qlist if t1[q] in list(set(t1_value)&set(t2_value))]
+                t2_r=dict(zip([s[1] for s in t2.items()],[s[0] for s in t2.items()]))
+                t2_qlist_new=[t2_r[s] for s in [t1[q] for q in t1_qlist_new]]
+                code1[c1]['qlist']=t1_qlist_new
+                code1[c1]['code']={k:t1[k] for k in t1_qlist_new}
+                qqlist1+=t1_qlist_new
+                qqlist2+=t2_qlist_new
+                qlist1.append(c1)
+                qlist2.append(rs_qq)
+                print('将自动合并: {} 和 {} (只保留了相同的选项)'.format(c1,rs_qq))
                 
+            elif similar_code in [1,2]:               
                 print('-'*40)
-                print('正在为第一份数据的  {}:{}  寻找匹配的题目....'.format(c1,code1[c1]['content']))
-                print('推荐合并:   {}:{}   ,其相似度为{:.0f}%.'.format(rs_qq,code2[rs_qq]['content'],similar))
-                user_qq=input('请输入第二份数据中与 {} 匹配的题号(缺省则表示跳过该题): '.format(c1))
-                user_qq=re.sub('\s','',user_qq)
-                user_qq=re.sub('^q','Q',user_qq)
-                if user_qq not in code2:
-                    print('将跳过该题..')
+                print('为【  {}:{} 】自动匹配到: '.format(c1,code1[c1]['content']))
+                print('  【  {}:{} 】,其相似度为{:.0f}%.'.format(rs_qq,code2[rs_qq]['content'],similar_content))
+                tmp=input('是否合并该组题目,请输入 yes/no (也可以输入第二份数据中其他您需要匹配的题目): ')
+                tmp=re.sub('\s','',tmp)
+                tmp=tmp.lower()
+                if tmp in ['yes','y']:
+                    user_qq=rs_qq
+                elif tmp in ['no','n']:
                     user_qq=None
-                elif (user_qq in code2) and (user_qq!=rs_qq):
-                    print('您输入的是{}:{}'.format(user_qq,code2[user_qq]['content']))
-                if user_qq:
+                else:
+                    tmp=re.sub('^q','Q',tmp)
+                    if tmp not in code2:
+                        user_qq=None
+                    elif (tmp in code2) and (tmp!=rs_qq):
+                        print('您输入的是{}:{}'.format(tmp,code2[tmp]['content']))
+                        user_qq=tmp
+                if user_qq==rs_qq:
+                    qqlist1+=code1[c1]['qlist']
+                    qqlist2+=code2[user_qq]['qlist']
+                    qlist1.append(c1)
+                    qlist2.append(user_qq)
+                    print('将自动合并: {} 和 {}'.format(c1,rs_qq))
+                elif user_qq is not None:
                     # 比对两道题目的code
-                    if qtype1=='单选题':
-                        t1=code1[c1]['code']
-                        t2=code2[user_qq]['code']
-                        inner_key=list(set(t1.keys())&set(t2.keys()))
-                        similar_code=all([t1[c]==t2[c] for c in inner_key])
-                    elif qtype1=='填空题':
-                        similar_code=True
-                    else: 
-                        similar_code=False
-                    if similar_code:
-                        print('将合并第一份数据的{}和第二份数据的{}...'.format(c1,user_qq))
-                        qlist1.append(c1)
-                        qlist2.append(user_qq)                       
+                    if 'code' in code1[c1] and len(code1[c1]['code'])>0:
+                        t1=code1[c1]['code_r'] if qtype1 =='矩阵单选题' else code1[c1]['code']
+                        t2=code2[user_qq]['code_r'] if code2[user_qq]['qtype'] =='矩阵单选题' else code2[user_qq]['code']
+                        if set(t1.values())==set(t2.values()):
+                            qqlist1+=code1[c1]['qlist']
+                            qqlist2+=code2[user_qq]['qlist']
+                            qlist1.append(c1)
+                            qlist2.append(user_qq)
+                            print('将自动合并: {} 和 {}'.format(c1,user_qq))
+                        else:
+                            print('两个题目的选项不匹配,将自动跳过.')
                     else:
-                        print('两题选项不匹配，将跳过该题(建议返回重新修改数据)')
+                        qqlist1+=[code1[c1]['qlist'][0]]
+                        qqlist2+=[code2[user_qq]['qlist'][0]]
+                        qlist1.append(c1)
+                        qlist2.append(user_qq)
+                        print('将自动合并: {} 和 {}'.format(c1,user_qq))
+                else:
+                    print('将自动跳过: {}'.format(c1))
                 print('-'*40)
             else:
-                print('将自动跳过: {}'.format(c1))      
+                print('将自动跳过: {}'.format(c1))
+        tmp=input('请问您需要的题目是否都已经合并? 请输入(yes / no)： ')
+        tmp=re.sub('\s','',tmp)
+        tmp=tmp.lower()
+        if tmp in ['no','n']:
+            print('请确保接下来您要合并的题目类型和选项完全一样.')
+            while 1:
+                tmp=input('请输入您想合并的题目对,直接回车则终止输入(如: Q1,Q1 ): ')
+                tmp=re.sub('\s','',tmp)# 去掉空格
+                tmp=re.sub('，',',',tmp)# 修正可能错误的逗号
+                tmp=tmp.split(',')
+                tmp=[re.sub('^q','Q',qq) for qq in tmp]
+                if len(tmp)<2:
+                    break
+                if tmp[0] in qlist1 or tmp[1] in qlist2:
+                    print('该题已经被合并，请重新输入')
+                    continue
+                if tmp[0] not in code1 or tmp[1] not in code2:
+                    print('输入错误, 请重新输入')
+                    continue
+                c1=tmp[0]
+                c2=tmp[1]
+                print('您输入的是：')
+                print('第一份数据中的【 {}:{} 】'.format(c1,code1[c1]['content']))
+                print('第二份数据中的【 {}:{} 】'.format(c2,code2[c2]['content']))
+                w=code_similar({c1:code1[c1]},{c2:code2[c2]})
+                similar_code=w.loc[c1,'similar_code']
+                if similar_code in [1,2] and len(code1[c1]['qlist'])==len(code2[c2]['qlist']):
+                    qqlist1+=code1[c1]['qlist']
+                    qqlist2+=code2[c2]['qlist']
+                    qlist1.append(c1)
+                    qlist2.append(c2)
+                    print('将自动合并: {} 和 {}'.format(c1,c2))
+                else:
+                    print('选项不匹配，请重新输入')                               
+        
+
+    else:
+        qqlist1=[]
+        for qq in qlist1:
+            qqlist1=qqlist1+code1[qq]['qlist']
+        qqlist2=[]
+        for qq in qlist2:
+            qqlist2=qqlist2+code2[qq]['qlist']
 
     # 将题号列表转化成data中的列名
-    if mergeqnum in qlist1:
+    if mergeqnum in qqlist1:
         mergeqnum=mergeqnum+'merge'
-    qqlist1=[]
-    for qq in qlist1:
-        qqlist1=qqlist1+code1[qq]['qlist']
-    data1=data1[qqlist1]
-    data1[mergeqnum]=1
-    
-    qqlist2=[]
-    for qq in qlist2:
-        qqlist2=qqlist2+code2[qq]['qlist']
-    data2=data2[qqlist2]
-    data2[mergeqnum]=2
+    data1=data1.loc[:,qqlist1]
+    data1.loc[:,mergeqnum]=1
+    data2=data2.loc[:,qqlist2]
+    data2.loc[:,mergeqnum]=2
     
     if len(qqlist1)!=len(qqlist2):
         print('两份数据选项不完全匹配，请检查....')
@@ -171,8 +328,19 @@ def data_merge(ques1,ques2,qlist1=None,qlist2=None,name1='ques1',name2='ques2',\
 
 data1,code1=rpt.wenjuanxing(['.\\data\\116_113_0.xls','.\\data\\116_113_2.xls'])
 data2,code2=rpt.wenjuanxing(['.\\data\\253_250_0.xls','.\\data\\253_250_2.xls'])
+'''
+ques1=[data1,code1]
+ques2=[data2,code2]
+qlist1=None
+qlist2=None
+name1='ques1'
+name2='ques2'
+mergeqnum='Q0'
+similar_threshold=70
+'''
 
 data12,code12=data_merge([data1,code1],[data2,code2])
+
 
 
 
